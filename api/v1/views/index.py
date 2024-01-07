b14d423d1c372fa3ba8f9f3e592073a6375fcb5c
Lwazi Ncube <lwazincubex@gmail.com>
2024-01-07 19:42:24 +0200

    Add documentation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,5 +4,8 @@ from api.v1.views import app_views
 
 @app_views.route('/status')
 def json_example():
+    """
+    returns a JSON response with status 'OK'.
+    """
     data = {"status": "OK"}
     return jsonify(data)
