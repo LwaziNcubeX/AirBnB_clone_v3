4b7392257d38f81591981de770a6d2f6beb51187
Lwazi Ncube <lwazincubex@gmail.com>
2024-01-07 20:22:42 +0200

    remove debug mode, simplified teardown method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def return_json():
     """
     returns a JSON response with status 'OK'.
